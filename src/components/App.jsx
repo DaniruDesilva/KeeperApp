69cd26fa3a8b6f2897e069d35263764a36b92780
Daniru De Silva <165991056+DaniruDesilva@users.noreply.github.com>
2024-06-01 19:15:27 +0530

    Keeper App Development-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import Header  from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App(){
     return(
         <div>
             <Header />
+            {notes.map(noteText => (
+            <Note 
+                key={noteText.key}
+                title={noteText.title}
+                content={noteText.content}
+               />
+            ))}
             <Footer />
-            <Note />
         </div>
     );
 }
